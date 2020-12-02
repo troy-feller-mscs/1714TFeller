b5b4a05ed439e730706bcac074fbbfaa922195ba
Troy Feller <ID3434QP@go.minnstate.edu>
2020-12-02 12:39:45 -0600

    Completed Ex3b

---
+++ 1714TFeller/1714tfellerex3b/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using _1714tfellerex2g;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _1714tfellerex3b
+{
+   
+    public partial class MainWindow : Window
+    {
+        PropertyManager2ModelContainer dbContext;
+        System.Windows.Data.CollectionViewSource personViewSource;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            personViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("personViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            dbContext = new PropertyManager2ModelContainer();
+            dbContext.People.Where(p => (p.Apartments1.Count > 0)).Load();
+            personViewSource.Source = dbContext.People.Local;
+            dbContext.Configuration.AutoDetectChangesEnabled = true;
+        }
+
+        private void firstButton_Click(object sender, RoutedEventArgs e)
+        {
+            personViewSource.View.MoveCurrentToFirst();
+        }
+
+        private void backButton_Click(object sender, RoutedEventArgs e)
+        {
+            personViewSource.View.MoveCurrentToPrevious();
+        }
+
+        private void nextButton_Click(object sender, RoutedEventArgs e)
+        {
+            personViewSource.View.MoveCurrentToNext();
+        }
+
+        private void lastButton_Click(object sender, RoutedEventArgs e)
+        {
+            personViewSource.View.MoveCurrentToLast();
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            dbContext.SaveChanges();
+        }
+    }
+}
---
+++ 1714TFeller/174tfellerex3b2/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using _1714tfellerex2g;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _174tfellerex3b2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        PropertyManager2ModelContainer dbContext;
+        System.Windows.Data.CollectionViewSource apartmentViewSource;
+        System.Windows.Data.CollectionViewSource apartmentInvoicesViewSource;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            apartmentViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("apartmentViewSource")));
+            apartmentInvoicesViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("apartmentInvoicesViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:v
+            dbContext = new PropertyManager2ModelContainer();
+            dbContext.Apartments.Load();
+            apartmentViewSource.Source = dbContext.Apartments.Local;
+            dbContext.Configuration.AutoDetectChangesEnabled = true;
+        }
+
+        private void firstButton_Click(object sender, RoutedEventArgs e)
+        {
+            apartmentViewSource.View.MoveCurrentToFirst();
+        }
+
+        private void backButton_Click(object sender, RoutedEventArgs e)
+        {
+            apartmentViewSource.View.MoveCurrentToPrevious();
+        }
+
+        private void nextButton_Click(object sender, RoutedEventArgs e)
+        {
+            apartmentViewSource.View.MoveCurrentToNext();
+        }
+
+        private void lastButton_Click(object sender, RoutedEventArgs e)
+        {
+            apartmentViewSource.View.MoveCurrentToLast();
+        }
+        private void firstInvoiceButton_Click(object sender, RoutedEventArgs e)
+        {
+            apartmentInvoicesViewSource.View.MoveCurrentToFirst();
+        } 
+        private void backInvoiceButton_Click(object sender, RoutedEventArgs e)
+        {
+            apartmentInvoicesViewSource.View.MoveCurrentToPrevious();
+        }
+        private void nextInvoiceButton_Click(object sender, RoutedEventArgs e)
+        {
+            apartmentInvoicesViewSource.View.MoveCurrentToNext();
+        }
+    private void lastInvoiceButton_Click(object sender, RoutedEventArgs e)
+        {
+            apartmentInvoicesViewSource.View.MoveCurrentToLast();
+        }
+    }
+}
